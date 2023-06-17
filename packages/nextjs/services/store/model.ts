6df3fcbd28b1850df3108c87e5ad3f8930b65cfd
Sergey Gerodes <sgerodes@gmail.com>
2023-06-17 15:35:41 +0200

    add store ts models

---
+++ packages/nextjs/services/store/model.ts
@@ -0,0 +1,9 @@
+export type JobListing = {
+    roleTitle: string;
+    description: string;
+    location?: string;
+    maxSalary?: number;
+    minSalary?: number;
+    bounty?: number;
+    companyName: string;
+  };
