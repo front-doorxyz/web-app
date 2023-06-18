edc512c9c25f1330e2094355591ffaeb040d608b
sergeyg <sergey.gerodes@raysono.com>
2023-06-18 09:59:06 +0200

    add compose DB types

---
+++ packages/nextjs/services/store/ceramic_models/SimpleProfile.json.js
@@ -0,0 +1,12 @@
+// This is an auto-generated file, do not edit manually
+export const definition = {
+    "models": {
+        "SimpleProfile": {
+            "id": "kjzl6hvfrbw6c6gidm618gwgcwzfx059qfzvdkxsmyt8ay2wku9ctzmqneip3x2",
+            "accountRelation": {"type": "single"}
+        }
+    },
+    "objects": {"SimpleProfile": {"displayName": {"type": "string", "required": true}}},
+    "enums": {},
+    "accountData": {"simpleProfile": {"type": "node", "name": "SimpleProfile"}}
+}
