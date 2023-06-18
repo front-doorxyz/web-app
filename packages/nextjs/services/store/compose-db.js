6645a7fe6a603a17c01e93163ac76d445b653fd9
sergeyg <sergey.gerodes@raysono.com>
2023-06-18 14:49:19 +0200

    update compose db queries

--- packages/nextjs/services/store/compose-db.js
+++ packages/nextjs/services/store/compose-db.js
@@ -2,24 +2,41 @@
 
 import { ComposeClient } from '@composedb/client'
 //import { definition } from "./compose_models/job-listing/JobListing-runtime-composite"
-import { definition } from "./compose_models/simple-profile/SimpleProfile-runtime-composite"
+import { definition } from "./compose_models/simple-profile/SimpleProfile-runtime-composite.js"
+// import { readEncodedComposite, writeEncodedComposite } from '@composedb/devtools-node'
 
 
 const ceramicUrl = process.env.CERAMIC_CLIENT_INSTANCE_URL || "http://localhost:7007";
 console.log("Ceramic client instance url to be used for compose DB:", ceramicUrl);
+const did = "0143f467e77d68ff5f9311241099872aa095674179eb6d503773502d1ec31255";
 
-const compose = new ComposeClient({ ceramic: ceramicUrl, definition });
+const ceramicClient = new ComposeClient({ ceramic: ceramicUrl, definition });
+ceramicClient.setDID(did);
+
 
 
 
 async function main() {
-    await compose.executeQuery(`
-  query {
-    viewer {
+    let response = await ceramicClient.executeQuery(`
+    mutation CreateNewSimpleProfile($i: CreateSimpleProfileInput!) {
+  createSimpleProfile(input: $i) {
+    document {
       id
+      displayName
     }
   }
-`)
+}
+
+`,
+        {
+            "i": {
+                "content": {
+                    "displayName": "John Doe"
+                }
+            }
+        }
+        )
+    console.log("Response:", response);
 }
 
 main().then(() => {console.log("Done")}).catch((err) => {console.error(err)})
