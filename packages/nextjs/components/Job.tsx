9662089cf0699d79237cab966b282362393d10c2
Fernando Ribas <fribas84@gmail.com>
2023-08-15 13:32:53 -0300

    refer button same side as apply

--- packages/nextjs/components/Job.tsx
+++ packages/nextjs/components/Job.tsx
@@ -53,7 +53,7 @@ const Job = (props: Props) => {
           </div>
         </div>
         <div className="flex items-center justify-center h-[20%] gap-2">
-          <button className="px-2 py-2 bg-blue-500 text-sm md:text-lg  text-white rounded" onClick={handleJobClick}>
+          <button className="px-4 py-2 bg-blue-500 text-sm md:text-lg  text-white rounded" onClick={handleJobClick}>
             Refer
           </button>
           <button className="px-4 py-2 bg-green-500 text-sm md:text-lg text-white rounded" onClick={handleJobClick}>
