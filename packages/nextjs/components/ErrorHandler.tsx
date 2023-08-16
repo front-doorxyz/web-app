81aa7bf29eeffb831e3ac2a680ceca6e210956ee
Fernando Ribas <fribas84@gmail.com>
2023-08-16 12:22:21 -0300

    form validation

--- packages/nextjs/components/ErrorHandler.tsx
+++ packages/nextjs/components/ErrorHandler.tsx
@@ -8,7 +8,7 @@ interface Props {
 const ErrorHandler = ({ showError, errorMsg }: Props) => (
   <>
     {showError && (
-      <div className="w-3/4 mt-5 mx-auto">
+      <div className="w-full mx-auto">
         <div className="text-white text-center  p-2 font-bold rounded-md bg-red-600 shadow-2xl">
           <p>{errorMsg}</p>
         </div>
--- packages/nextjs/pages/contactus/index.tsx
+++ packages/nextjs/pages/contactus/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { NextPage } from "next";
+import ErrorHandler from "~~/components/ErrorHandler";
 
 interface IErrors {
   fullname: boolean;
@@ -39,10 +40,14 @@ const ContactUs: NextPage = () => {
       isValid = false;
     }
     setErrors(tempErrors);
+    if(!isValid){
+      console.log(tempErrors);
+    }
     return isValid;
+    
   };
 
-  const handleSubmit = async e => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const isFormValid = handleValidation();
     if (isFormValid) {
@@ -76,34 +81,51 @@ const ContactUs: NextPage = () => {
 
   return (
     <div className="flex flex-col items-center  mt-[2%]">
-      <div id="info" className="flex flex-col items-center justify-center gap-2">
-        <h3 className="text-sm md:text-xl">Get in touch</h3>
-        <h1 className="text-xl md:text-5xl font-bold font-bai-jamjuree text-black dark:text-gray-500">
-          We&apos;d love to hear from you
-        </h1>
-      </div>
-      <div
-        id="form"
-        className="w-[300px] md:w-[30vw] bg-white rounded-[2%] p-10 mt-[4%] flex flex-col items-center justify-center gap-5 "
-      >
-        <h3 className="text-sm md:text-xl  text-black ">Feel free to reach out</h3>
-        <input type="text" placeholder="Enter your name" className="input input-bordered  w-[200px] md:w-[20vw] " />
-        <input type="text" placeholder="Enter your email" className="input input-bordered w-[200px] md:w-[20vw] " />
-        <textarea
-          placeholder="Enter your message"
-          className="peer h-3/4 min-h-[100px] w-[200px] md:w-[20vw] input input-bordered p-3"
-        />
-        <button
-          disabled={enableSubmit ? false : true}
-          className={
-            enableSubmit
-              ? "px-8 py-3 btn btn-primary text-white rounded focus:outline-none w-[200px] md:w-[20vw] hover:scale-110 "
-              : "px-8 py-3 bg-slate-500 text-white rounded focus:outline-none w-[200px] md:w-[20vw]"
-          }
+      <form onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}>
+        <div id="info" className="flex flex-col items-center justify-center gap-2">
+          <h3 className="text-sm md:text-xl">Get in touch</h3>
+          <h1 className="text-xl md:text-5xl font-bold font-bai-jamjuree text-black dark:text-gray-500">
+            We&apos;d love to hear from you
+          </h1>
+        </div>
+        <div
+          id="form"
+          className="w-[300px] mx-auto md:w-[30vw] bg-white rounded-[2%] p-10 mt-[4%] flex flex-col items-center justify-center gap-5 "
         >
-          {submitButtonText}
-        </button>
-      </div>
+          <h3 className="text-sm md:text-xl  text-black ">Feel free to reach out</h3>
+          {errors.fullname && <ErrorHandler showError={errors.fullname} errorMsg="Name cannot be empty." />}
+          {errors.email && <ErrorHandler showError={errors.email} errorMsg="Invalid email." />}
+          {errors.message && <ErrorHandler showError={errors.message} errorMsg="Message cannot be empty." />}
+          <input
+            type="text"
+            placeholder="Enter your name"
+            className="input input-bordered  w-[200px] md:w-[20vw]"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setFullname(e.currentTarget.value)}
+          />
+          <input
+            type="text"
+            placeholder="Enter your email"
+            className="input input-bordered w-[200px] md:w-[20vw]"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.currentTarget.value)}
+          />
+          <textarea
+            placeholder="Enter your message"
+            className="peer h-3/4 min-h-[100px] w-[200px] md:w-[20vw] input input-bordered p-3"
+            onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setMessage(e.currentTarget.value)}
+          />
+          <button
+            disabled={enableSubmit ? false : true}
+            className={
+              enableSubmit
+                ? "px-8 py-3 btn btn-primary text-white rounded focus:outline-none w-[200px] md:w-[20vw] hover:scale-110 "
+                : "px-8 py-3 bg-slate-500 text-white rounded focus:outline-none w-[200px] md:w-[20vw]"
+            }
+            type="submit"
+          >
+            {submitButtonText}
+          </button>
+        </div>
+      </form>
       {showSuccessMessage && (
         <p className="text-green-500 font-semibold text-sm my-2">Thank you! Your Message has been delivered.</p>
       )}
