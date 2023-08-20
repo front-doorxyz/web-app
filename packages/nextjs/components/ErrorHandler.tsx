bb7a85c8e73260f106a0378192312ce65735c086
Fernando Ribas <fribas84@gmail.com>
2023-08-20 07:11:28 -0300

    added contact form

---
+++ packages/nextjs/components/ErrorHandler.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface Props {
+  showError: boolean;
+  errorMsg: string;
+}
+
+const ErrorHandler = ({ showError, errorMsg }: Props) => (
+  <>
+    {showError && (
+      <div className="w-full mx-auto">
+        <div className="text-white text-center  p-2 font-bold rounded-md bg-red-600 shadow-2xl">
+          <p>{errorMsg}</p>
+        </div>
+      </div>
+    )}
+  </>
+);
+
+export default ErrorHandler;
