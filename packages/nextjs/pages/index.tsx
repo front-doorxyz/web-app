f873412ddfb36b3fa62a586c2c1b65f6f361f769
Fernando Ribas <fribas84@gmail.com>
2023-09-07 17:51:42 -0300

    removed snaps hooks from index

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,21 +1,19 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import detectEthereumProvider from "@metamask/detect-provider";
+
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
 import Banner from "~~/components/Banner";
 import ErrorHandler from "~~/components/ErrorHandler";
 import Jobs from "~~/components/Jobs";
-import { MetaMaskContext, MetamaskActions } from "~~/hooks/MetamaskContext";
 import { getAllJobsOfCompany } from "~~/services/APIs/smartContract";
-import { connectSnap, getSnap } from "~~/utils/snap";
+
 
 const AllJobs: NextPage = () => {
   const { address } = useAccount();
   const router = useRouter();
   const [showError, setShowError] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string>("");
-  const [state, dispatch] = useContext(MetaMaskContext);
   useEffect(() => {
     // Extract the query parameters from the URL
     const { refId, jobId } = router.query;
@@ -32,34 +30,7 @@ const AllJobs: NextPage = () => {
       getAllJobsOfCompany(address);
     }
   }, [address]);
-  useEffect(() => {
-    const dectectProvider = async () => {
-      const provider = await detectEthereumProvider();
-
-      // web3_clientVersion returns the installed MetaMask version as a string
-      const isFlask = (await provider?.request({ method: "web3_clientVersion" }))?.includes("flask");
-
-      if (!isFlask) {
-        setShowError(true);
-        setErrorMessage("Please install MetaMask Flask to use this application");
-      } else {
-        (async () => {
-          try {
-            await connectSnap();
-            const installedSnap = await getSnap();
-            dispatch({
-              type: MetamaskActions.SetInstalled,
-              payload: installedSnap,
-            });
-          } catch (e) {
-            console.error(e);
-            dispatch({ type: MetamaskActions.SetError, payload: e });
-          }
-        })();
-      }
-    };
-    dectectProvider();
-  }, []);
+  
   return (
     <>
       <Banner />
